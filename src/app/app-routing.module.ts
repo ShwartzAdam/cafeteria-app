12a1347e94079e890fca4a858e089967e0e0f7f1
Adam Shwartz <adam@shwartz.co>
2018-03-02 08:26:08 +0200

    login , forgotpass, signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // Components for routes
 import {LoginComponent} from './auth/login/login.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {ForgotpassComponent} from './auth/forgotpass/forgotpass.component';
 
 
 const appRoutes: Routes = [
-  { path: '', redirectTo : '/home' , pathMatch : 'full' },
-  { path: 'home', component: LoginComponent},
-
+  { path: '' , redirectTo : '/home' , pathMatch : 'full' },
+  { path: 'home' , component : LoginComponent},
+  { path: 'signup' , component : SignupComponent},
+  { path: 'forgotpass' , component : ForgotpassComponent}
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(appRoutes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// Modules
+// Modules Added
 import { NgSemanticModule } from 'ng-semantic/ng-semantic';
 import { AppRoutingModule } from './app-routing.module';
 // Main Component
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 // Sub Components
 import { MenuComponent } from './menu.component';
 import { LoginComponent } from './auth/login/login.component';
+import { LoginViewComponent } from './auth/login/login-view/login-view.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ForgotpassComponent } from './auth/forgotpass/forgotpass.component';
 
 
 @NgModule({
@@ -15,6 +18,9 @@ import { LoginComponent } from './auth/login/login.component';
     AppComponent,
     MenuComponent,
     LoginComponent,
+    LoginViewComponent,
+    SignupComponent,
+    ForgotpassComponent,
   ],
   imports: [
     BrowserModule,
