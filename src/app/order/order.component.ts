400041074c568709de9bfaa13ab6f821b2a15b4a
Adam Shwartz <adam@shwartz.co>
2018-06-13 17:41:26 +0300

    changesss

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { OrderListService } from '../services/orderlist.service';
 
+declare var jQuery: any;
 
 @Component({
   selector: 'app-order',
