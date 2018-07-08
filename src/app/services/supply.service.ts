619186e933fe34210917e4bc74058cb7edf13278
Adam Shwartz <adam@shwartz.co>
2018-07-08 18:22:01 +0300

    changes

--- src/app/services/supply.service.ts
+++ src/app/services/supply.service.ts
@@ -10,7 +10,7 @@ import {UserData} from './user-data/user-data.service';
 @Injectable()
 export class SupplyService {
   public url = 'https://cafeappserver.herokuapp.com/api';
-  public urlDev = 'http://localhost:3000/api';
+  public urlPrd = 'http://localhost:3000/api';
   public headerConfig: any;
   constructor(private http: HttpClient) {
     // this.setToken();
